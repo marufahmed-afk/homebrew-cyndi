01004729e222d469f9163c2b4ebc822a5213e86b
Maruf Ahmed <55205290+marufahmed-afk@users.noreply.github.com>
2026-07-26 12:38:04 +0100

    cyndi 0.2.1

--- Casks/cyndi.rb
+++ Casks/cyndi.rb
@@ -1,13 +1,13 @@
 cask "cyndi" do
-  version "0.2.0"
-  sha256 "ef99ee8c19292d13de74813bd2ac612a894a19f45c8b996af72a525b269f9612"
+  version "0.2.1"
+  sha256 "2847f3091e290d16252a7d5db6e540d7e62711aee9acfce32b1e28619ec903db"
 
   url "https://github.com/marufahmed-afk/cyndi/releases/download/v#{version}/Cyndi.dmg"
   name "Cyndi"
   desc "Sticky notes that live at the notch"
   homepage "https://github.com/marufahmed-afk/cyndi"
 
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "Cyndi.app"
 
