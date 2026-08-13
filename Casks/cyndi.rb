fba20712790768104f092da627091b96b7e938b4
Maruf Ahmed <maruf.ahmed@ballys.com>
2026-08-13 10:31:27 +0100

    fix: serve DMG from public tap repo so brew can download it

--- Casks/cyndi.rb
+++ Casks/cyndi.rb
@@ -2,7 +2,7 @@ cask "cyndi" do
   version "0.3.1"
   sha256 "a64ca76a6d1a87fd1e38003d92b38ad442f609f41261744019d88ea952bf1d51"
 
-  url "https://github.com/marufahmed-afk/cyndi/releases/download/v#{version}/Cyndi.dmg"
+  url "https://github.com/marufahmed-afk/homebrew-cyndi/releases/download/v#{version}/Cyndi.dmg"
   name "Cyndi"
   desc "Sticky notes that live at the notch"
   homepage "https://github.com/marufahmed-afk/cyndi"
